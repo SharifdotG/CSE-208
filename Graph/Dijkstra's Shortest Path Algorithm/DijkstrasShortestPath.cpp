36452d47a974d99fe83c6207d1f169a9cb4cf4de
Sharif Yousuf <sharifmdyousuf007@gmail.com>
2024-03-24 20:24:10 +0600

    Update user authentication logic

---
+++ Graph/Dijkstra's Shortest Path Algorithm/DijkstrasShortestPath.cpp
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int minDistance(int dist[], bool sptSet[], int V) {
+    int min = INT_MAX, min_index;
+
+    for (int v = 0; v < V; v++) {
+        if (!sptSet[v] && dist[v] <= min) {
+            min = dist[v];
+            min_index = v;
+        }
+    }
+
+    return min_index;
+}
+
+void printSolution(int dist[], int V) {
+    cout << "Vertex \t Distance from Source" << endl;
+    for (int i = 0; i < V; i++) {
+        cout << i << " \t\t " << dist[i] << endl;
+    }
+}
+
+void dijkstra(int graph[9][9], int src, int V) {
+    int dist[V];
+    bool sptSet[V];
+
+    for (int i = 0; i < V; i++) {
+        dist[i] = INT_MAX;
+        sptSet[i] = false;
+    }
+
+    dist[src] = 0;
+
+    for (int count = 0; count < V - 1; count++) {
+        int u = minDistance(dist, sptSet, V);
+
+        sptSet[u] = true;
+
+        for (int v = 0; v < V; v++) {
+            if (!sptSet[v] && graph[u][v] && dist[u] != INT_MAX && dist[u] + graph[u][v] < dist[v]) {
+                dist[v] = dist[u] + graph[u][v];
+            }
+        }
+    }
+
+    printSolution(dist, V);
+}
+
+int main() {
+    int graph[9][9] = {
+        {0, 4, 0, 0, 0, 0, 0, 8, 0},
+        {4, 0, 8, 0, 0, 0, 0, 11, 0},
+        {0, 8, 0, 7, 0, 4, 0, 0, 2},
+        {0, 0, 7, 0, 9, 14, 0, 0, 0},
+        {0, 0, 0, 9, 0, 10, 0, 0, 0},
+        {0, 0, 4, 14, 10, 0, 2, 0, 0},
+        {0, 0, 0, 0, 0, 2, 0, 1, 6},
+        {8, 11, 0, 0, 0, 0, 1, 0, 7},
+        {0, 0, 2, 0, 0, 0, 6, 7, 0}
+    };
+
+    dijkstra(graph, 0, 9);
+
+    return 0;
+}
