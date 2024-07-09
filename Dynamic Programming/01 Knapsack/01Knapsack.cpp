794724e06ee6f3c32b0c65fb8dfb6261cb9d8f9e
Sharif Yousuf <sharifmdyousuf007@gmail.com>
2024-07-10 02:43:34 +0600

    Refactor knapsack implementation for improved performance

--- Dynamic Programming/01 Knapsack/01Knapsack.cpp
+++ Dynamic Programming/01 Knapsack/01Knapsack.cpp
@@ -4,13 +4,14 @@ using namespace std;
 
 int knapsack(int weights[], int values[], int n, int capacity) {
     int dp[n + 1][capacity + 1];
-    
+
     for (int i = 0; i <= n; i++) {
         for (int w = 0; w <= capacity; w++) {
             if (i == 0 || w == 0) {
                 dp[i][w] = 0;
             } else if (weights[i - 1] <= w) {
-                dp[i][w] = max(values[i - 1] + dp[i - 1][w - weights[i - 1]], dp[i - 1][w]);
+                dp[i][w] = max(values[i - 1] + dp[i - 1][w - weights[i - 1]],
+                               dp[i - 1][w]);
             } else {
                 dp[i][w] = dp[i - 1][w];
             }
